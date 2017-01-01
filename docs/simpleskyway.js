ded562856c1c94c1f7ebf82139f8577e854269b7
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-01-02 03:02:25 +0900

    commit

--- docs/simpleskyway.js
+++ docs/simpleskyway.js
@@ -71,6 +71,98 @@ signalingChannelOnMessage = evt => {
         start();
 
     var message = JSON.parse(evt.data);
+    if (message.type) {
+        var payload = message.payload;
+        var peer = message.src;
+        var connection;
+        switch (message.type) {
+            case 'OPEN': // The connection to the server is open.
+                //this.emit('signalingChannel open', this.id);
+                //this.open = true;
+                console.log('sc open');
+                break;
+            case 'ERROR': // Server error.
+                console.log('server-error', payload.msg);
+                break;
+            case 'ID-TAKEN': // The selected ID is taken.
+                console.log('unavailable-id', `ID "${this.id}" is taken`);
+                break;
+            case 'INVALID-KEY': // The given API key cannot be found.
+                console.log('invalid-key', `API KEY "${this.options.key}" is invalid`);
+                break;
+            case 'PING':
+                this.socket.sendPong();
+                break;
+            case 'LEAVE': // Another peer has closed its connection to this peer.
+                console.log('Received leave message from', peer);
+                this._cleanupPeer(peer);
+                break;
+
+            case 'EXPIRE': // The offer sent to a peer has expired without response.
+                this.emitError('peer-unavailable', 'Could not connect to peer ' + peer);
+                break;
+            case 'OFFER': // we should consider switching this to CALL/CONNECT, but this is the least breaking option.
+                var connectionId = payload.connectionId;
+                connection = this.getConnection(peer, connectionId);
+
+                if (connection) {
+                    console.warn('Offer received for existing Connection ID:', connectionId);
+                    //connection.handleMessage(message);
+                } else {
+                    // Create a new connection.
+                    if (payload.type === 'media') {
+                        connection = new MediaConnection(peer, this, {
+                            connectionId: connectionId,
+                            _payload: payload,
+                            metadata: payload.metadata
+                        });
+                        util.log("MediaConnection created in OFFER");
+                        this._addConnection(peer, connection);
+                        this.emit('call', connection);
+                    } else if (payload.type === 'data') {
+                        connection = new DataConnection(peer, this, {
+                            connectionId: connectionId,
+                            _payload: payload,
+                            metadata: payload.metadata,
+                            label: payload.label,
+                            serialization: payload.serialization,
+                            reliable: payload.reliable
+                        });
+                        this._addConnection(peer, connection);
+                        this.emit('connection', connection);
+                    } else {
+                        util.warn('Received malformed connection type:', payload.type);
+                        return;
+                    }
+                    // Find messages.
+                    var messages = this._getMessages(connectionId);
+                    for (var i = 0, ii = messages.length; i < ii; i += 1) {
+                        connection.handleMessage(messages[i]);
+                    }
+                }
+                break;
+            default:
+                if (!payload) {
+                    console.warn('You received a malformed message from ' + peer + ' of type ' + type);
+                    return;
+                }
+
+                var id = payload.connectionId;
+                connection = this.getConnection(peer, id);
+
+                if (connection && connection.pc) {
+                    // Pass it on.
+                    connection.handleMessage(message);
+                } else if (id) {
+                    // Store for possible later use
+                    this._storeMessage(id, message);
+                } else {
+                    console.warn('You received an unrecognized message:', message);
+                }
+                break;
+        }
+    }
+
     if (message.desc) {
         var desc = message.desc;
 
